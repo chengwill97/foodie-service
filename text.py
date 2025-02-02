22f5bf0fcdacf36d61e34133d5d26c11fc40759e
William Cheng <chengwill97@gmail.com>
2025-02-01 23:30:10 -0500

    add os env to text.py

--- text.py
+++ text.py
@@ -1,18 +1,17 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
-import sys
 from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = sys.argv[1]
-auth_token = sys.argv[2]
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-    from_="+18447873896",
-    to="+12678797686",
+    from_=os.environ['FROM_NUMBER'],
+    to=os.enrivon['TO_NUMBER'],
 )
 
 print(message.body)
